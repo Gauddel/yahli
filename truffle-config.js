ee07e9d3128cd1e853186a51df481fbf4ebc3964
Shivva <matthieumariejoseph@gmail.com>
2020-08-31 22:00:18 +0200

    little patch

--- truffle-config.js
+++ truffle-config.js
@@ -10,15 +10,7 @@ module.exports = {
       port: 7545,
       network_id : "*"
     },
-    kovan: {
-      provider: () => new HDWalletProvider("enlist dish undo van potato gate hair champion denial elegant echo edge", `https://kovan.infura.io/v3/3d536147812b49c280108388ceb563ce`),
-      network_id: 42,       // Ropsten's id
-      gas: 5500000,        // Ropsten has a lower block limit than mainnet
-      confirmations: 2,    // # of confs to wait between deployments. (default: 0)
-      timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
-      skipDryRun: true,    // Skip dry run before migrations? (default: false for public nets )
-      gasPrice: 20000000000
-    },
+
   },
   compilers: {
     solc: {
